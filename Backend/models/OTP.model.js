19bd8e16995be7d470834ecf031819a711d200c0
Rajan <rajanmasih154@gmail.com>
2023-11-16 03:08:20 +0530

    Creating OTP sender

--- Backend/models/OTP.model.js
+++ Backend/models/OTP.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender.utils");
 
 const otpSchema = new mongoose.Schema({
   email: {
@@ -15,5 +16,22 @@ const otpSchema = new mongoose.Schema({
     expires: 5 * 60,
   },
 });
+
+// Function for sending email
+
+async function sendVerificationEmail(email,otp) {
+  try {
+    const mailResponse = await mailSender(email, "Verification for Email", otp);
+    console.log("Otp send successfully", mailResponse);
+  } catch (err) {
+    console.error("Error in send verification function in otp model" ,err);
+    throw err;
+  }
+}
+
+otpSchema.pre("save", async function(next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+})
 
 module.exports = mongoose.model("Otp", otpSchema);
