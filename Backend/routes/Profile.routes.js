bf8146e50aef72a3f5bbf5df53bbeede49ce901b
Rajan <rajanmasih154@gmail.com>
2023-11-28 00:50:02 +0530

    Routes added

---
+++ Backend/routes/Profile.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { auth } = require("../middlewares/Auth.middleware");
+
+const {
+  updateProfile,
+  deleteProfile,
+  getAllDetails,
+  updateDisplayPicture,
+  getEnrolledCourse,
+} = require("../controllers/Profile.controller");
+
+// **************************************************************
+//                      Profile's Routes
+// **************************************************************
+
+// Delete Account
+router.delete("/deleteProfile", deleteProfile);
+router.put("/updateProfile", auth, updateProfile);
+router.get("/getAllDetails", auth, getAllDetails);
+
+// Get Enrolled Courses
+router.get("/getEnrolledCourse", auth, getEnrolledCourse);
+router.put("/updateDisplayPicture", auth, updateDisplayPicture);
+
+module.exports = router;
