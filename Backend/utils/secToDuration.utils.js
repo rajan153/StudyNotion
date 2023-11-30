3ba5a038cd233e1bcd392841eb5bd373cfa60e46
Rajan <rajanmasih154@gmail.com>
2023-11-30 17:55:59 +0530

    seconds to duration function added

---
+++ Backend/utils/secToDuration.utils.js
@@ -0,0 +1,16 @@
+// This function helps to convert seconds to duration time format
+function convertSecondsToDuration(totalSeconds) {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor(totalSeconds / 3600 / 60);
+  const seconds = Math.floor((totalSeconds / 3600) % 60);
+
+  if (hours > 0) {
+    return `${hours}h ${minutes}m`;
+  } else if (minutes > 0) {
+    return `${minutes}m ${seconds}s`;
+  } else {
+    return `${seconds}s`;
+  }
+}
+
+module.exports = { convertSecondsToDuration };
