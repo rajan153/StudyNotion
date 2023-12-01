ae8c499bdcc4802b7f75b1796b90a6dc5c6592c8
Rajan <rajanmasih154@gmail.com>
2023-12-02 00:49:33 +0530

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import HomePages from "./pages/HomePages";
 
+function App() {
   return (
-    <>
-      <h1 className='bg-black text-white text-center'>Hello Rajan</h1>
-    </>
-  )
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter items-center">
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
