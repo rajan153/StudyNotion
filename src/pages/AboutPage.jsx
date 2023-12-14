1369f461d2b65aa238edd7e22b20f7e77397be6d
Rajan <rajanmasih154@gmail.com>
2023-12-14 23:21:27 +0530

    About page added

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import About from "../components/core/AboutPage/About"
+
+function AboutPage() {
+  return (
+    <div className='w-full'>
+        <About />
+    </div>
+  )
+}
+
+export default AboutPage
