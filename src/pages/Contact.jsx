bf71f87d914dc910c625275ef496e14030ba9ee2
Rajan <rajanmasih154@gmail.com>
2023-12-14 23:19:59 +0530

    Contact page added

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ContactDetails from "../components/core/contactPage/ContactDetails";
+import ContactFormSection from "../components/core/AboutPage/ContactFormSection";
+
+function Contact() {
+  return (
+    <div className="w-full">
+      <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row">
+        {/* Contact Details */}
+        <div className="lg:w-[40%]">
+          <ContactDetails />
+        </div>
+        {/* Contact Form */}
+        <div className="lg:w-[60%]">
+          <ContactFormSection />
+        </div>
+      </div>
+      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+        {/* Reviws from Other Learner */}
+        <h2 className="text-center text-4xl font-semibold mt-8">
+          Reviews from other learners
+        </h2>
+        {/* <ReviewSlider /> */}
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
