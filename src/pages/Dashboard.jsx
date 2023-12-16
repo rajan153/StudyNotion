40e3bb4edcd24c5110f511d0f0e4659d462c59b3
Rajan <rajanmasih154@gmail.com>
2023-12-17 00:20:59 +0530

    dashboard added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import PrivateRoute from "../components/common/PrivateRoute";
+import { useSelector } from "react-redux";
+import SideBar from "../components/core/Dashboard/SideBar"
+
+function Dashboard() {
+  <PrivateRoute />;
+  const { loading: profileLoading } = useSelector((state) => state.profile);
+  const { loading: authLoading } = useSelector((state) => state.auth);
+
+  if (profileLoading || authLoading) {
+    return (
+      <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+  return (
+    <div className="relative flex min-h-[calc(100vh-3.5rem)] w-full">
+      <SideBar />
+      <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+        <div className="mx-auto w-11/12 max-w-[1000px] py-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
