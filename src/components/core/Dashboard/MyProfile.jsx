586393d4d1cee3540cbf11321d425bb5b57f48e2
Rajan <rajanmasih154@gmail.com>
2023-12-18 19:52:56 +0530

    profile added

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -15,7 +15,7 @@ function MyProfile() {
       <div className="flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
         <div className="flex items-center gap-x-4">
           <img
-            src={user?.image}
+            src={user?.images}
             alt={`profile-${user?.firstName}`}
             className="aspect-square w-[78px] rounded-full object-cover"
           />
