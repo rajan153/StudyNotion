ccbe5e43aaf1e0fb8312cd336ec6b632918a4294
Rajan <rajanmasih154@gmail.com>
2023-12-27 02:46:26 +0530

    fix code of courseProgress

--- Backend/controllers/Payment.controller.js
+++ Backend/controllers/Payment.controller.js
@@ -183,7 +183,7 @@ const enrollStudents = async (courses, userId, res) => {
       console.log("Updated course: ", enrolledCourse);
 
       const courseProgress = await CourseProgress.create({
-        courseID: courseId,
+        courseId: courseId,
         userId: userId,
         completedVideos: [],
       });
