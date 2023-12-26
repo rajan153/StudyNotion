62c6f298173594fe5565dc08633f81ccdf5f41cd
Rajan <rajanmasih154@gmail.com>
2023-12-26 23:40:16 +0530

    update courseSlice

--- src/slices/viewCourseSlice.js
+++ src/slices/viewCourseSlice.js
@@ -1,41 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit"
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    courseSectionData: [],
-    courseEntireData: [],
-    completedLectures: [],
-    totalNoOfLectures: 0,
-}
+  courseSectionData: [],
+  courseEntireData: [],
+  completedLectures: [],
+  totalNoOfLectures: 0,
+};
 
 const viewCourseSlice = createSlice({
-    name:"viewCourse",
-    initialState,
-    reducers: {
-        setCourseSectionData:(state,action) => {
-            state.courseSectionData = action.payload;
-        },
-        setCourseEntireData:(state,action) => {
-            state.courseEntireData = action.payload;
-        },
-        setCompletedLectures: (state,action) => {
-            state.completedLectures = action.payload;
-        },
-        setTotalNoOfLectures: (state, action) => {
-            state.totalNoOfLectures = action.payload;
-        },
-        updateCompletedLectures:(state,action) => {
-            state.completedLectures = [...state.completedLectures,action.payload]
-        },
-    }
-})
+  name: "viewCourse",
+  initialState,
+  reducers: {
+    setCourseSectionData: (state, action) => {
+      state.courseSectionData = action.payload;
+    },
+    setCourseEntireData: (state, action) => {
+      state.courseEntireData = action.payload;
+    },
+    setCompletedLectures: (state, action) => {
+      state.completedLectures = action.payload;
+    },
+    setTotalNoOfLectures: (state, action) => {
+      state.totalNoOfLectures = action.payload;
+    },
+    updateCompletedLectures: (state, action) => {
+      state.completedLectures = [...state.completedLectures, action.payload];
+    },
+  },
+});
 
-const {
-    setCourseSectionData,
-    setCourseEntireData,
-    setTotalNoOfLectures,
-    setCompletedLectures,
-    updateCompletedLectures
-} = viewCourseSlice.actions
+export const {
+  setCourseSectionData,
+  setCourseEntireData,
+  setTotalNoOfLectures,
+  setCompletedLectures,
+  updateCompletedLectures,
+} = viewCourseSlice.actions;
 
-export default viewCourseSlice.reducer
+export default viewCourseSlice.reducer;
