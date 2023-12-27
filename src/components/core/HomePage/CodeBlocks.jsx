3e4070a459c5d19ee5bc262bc651d5c7a1685317
Rajan <rajanmasih154@gmail.com>
2023-12-27 14:01:50 +0530

    fixed responsive

--- src/components/core/HomePage/CodeBlocks.jsx
+++ src/components/core/HomePage/CodeBlocks.jsx
@@ -14,13 +14,17 @@ function CodeBlocks({
   codeColour,
 }) {
   return (
-    <div className={`flex ${position} my-20 justify-between gap-10 lg:gap-10`}>
+    <div
+      className={`flex ${position} mt-8 lg:my-20 justify-between flex-col gap-10`}
+    >
       {/* Section 1 */}
-      <div className="w-[50%] flex flex-col gap-8">
+      <div className="w-[100%] lg:w-[50%] flex flex-col gap-8">
         {heading}
-        <div className="text-richblack-300 font-medium text-[16px] leading-6">{subHeading}</div>
+        <div className="text-richblack-300 text-base font-bold w-[85%] -mt-3">
+          {subHeading}
+        </div>
         {/* Buttons */}
-        <div className="flex gap-7 mt-7 text-white">
+        <div className="flex gap-7 mt-7">
           <CTAButton active={ctaBtn1.active} linkto={ctaBtn1.linkto}>
             <div className="flex items-center gap-2">
               {ctaBtn1.btnText}
@@ -33,14 +37,10 @@ function CodeBlocks({
         </div>
       </div>
       {/* Section 2 */}
-      <div className="h-fit flex flex-row text-[10px] w-[100%] py-3 code-border leading-[18px]
-      sm:leading-6 sm:text-sm relative lg:w-[450px]">
+      <div className="h-fit code-border flex flex-row py-3 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-[100%] lg:w-[470px]">
         {/* BackGround Gradient */}
         {backgroundGradient}
-        <div
-          className="flex flex-col text-center w-[10%] text-richblack-400 font-inter
-          font-bold select-none"
-        >
+        <div className="text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold select-none">
           <p>1</p>
           <p>2</p>
           <p>3</p>
@@ -53,12 +53,14 @@ function CodeBlocks({
           <p>10</p>
           <p>11</p>
         </div>
-        <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColour} pr-1 leading-6 text-[14px]`}>
-          <TypeAnimation 
+        <div
+          className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColour} pr-2`}
+        >
+          <TypeAnimation
             sequence={[codeBlocks, 1000, ""]}
             cursor={true}
             repeat={Infinity}
-            style={{whiteSpace:"pre-line", display:"block"}}
+            style={{ whiteSpace: "pre-line", display: "block" }}
             omitDeletionAnimation={true}
           />
         </div>
